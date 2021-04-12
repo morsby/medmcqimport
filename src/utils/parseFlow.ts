3451b845ec2071b0b36e33367d79c84723883c09
Morsby <sigurd.morsby@gmail.com>
2021-04-12 19:55:58 +0200

    Support for image blobs

--- src/utils/parseFlow.ts
+++ src/utils/parseFlow.ts
@@ -52,40 +52,59 @@ class ExamSet {
     return `${this.semesterName}-${this.year}${this.season}`;
   }
 
- async downloadImages() {
+  async downloadImages() {
     if (this.semesterId === 4) {
       console.log(colors.yellow("Fjerner billeder, da det er 11. semester"));
-      this.questions = this.questions.map(question => {
+      this.questions = this.questions.map((question) => {
         question.images = [];
         return question;
       });
     } else {
-      this.questions = await Promise.all(this.questions.map(async question => {
-        if (question.images.length > 0) {
-          let imgDir = rootPath + "/output/images";
-          if (!fs.existsSync(imgDir)) fs.mkdirSync(imgDir, { recursive: true });
-          const imageName = `${this.stringifySetInfo()}-${
-            question.examSetQno
-          }.jpg`;
-          
-          await request({ uri: question.images[0].link, encoding: "binary" })
-          .then(body => {
-              const imageFile = fs.createWriteStream(`${imgDir}/${imageName}`);
-              imageFile.write(body, "binary");
-              imageFile.end();
-              question.images[0].link = imageName;
-            })
-            .catch(() => {
-              console.error(
-                colors.red(
-                  "Kunne ikke hente billede til spørgsmål " +
-                  question.examSetQno
-                  )
+      this.questions = await Promise.all(
+        this.questions.map(async (question) => {
+          if (question.images.length > 0) {
+            let imgDir = rootPath + "/output/images";
+            if (!fs.existsSync(imgDir)) fs.mkdirSync(imgDir, { recursive: true });
+            const imageName = `${this.stringifySetInfo()}-${question.examSetQno}`;
+
+            // image is blob
+            if (question.images[0].link.includes("data:image")) {
+              // Grab the extension to resolve any image error
+              // ugly possible null fix
+              var ext = (question.images[0].link.split(";")[0].match(/jpeg|png|gif/) || [
+                null,
+              ])[0];
+              // strip off the data: url prefix to get just the base64-encoded bytes
+              var data = question.images[0].link.replace(/^data:image\/\w+;base64,/, "");
+              var buf = Buffer.from(data, "base64");
+              fs.writeFile(
+                `${imgDir}/${imageName}.${ext}`,
+                buf,
+                "base64",
+                function (err) {
+                  console.log(err);
+                }
+              );
+            } else {
+              await request({ uri: question.images[0].link, encoding: "binary" })
+                .then((body) => {
+                  const imageFile = fs.createWriteStream(`${imgDir}/${imageName}.jpg`);
+                  imageFile.write(body, "binary");
+                  imageFile.end();
+                  question.images[0].link = imageName;
+                })
+                .catch(() => {
+                  console.error(
+                    colors.red(
+                      "Kunne ikke hente billede til spørgsmål " + question.examSetQno
+                    )
                   );
                 });
-              }
-        return question;
-      }));
+            }
+          }
+          return question;
+        })
+      );
     }
   }
 
@@ -94,13 +113,12 @@ class ExamSet {
       semesterId: this.semesterId,
       season: this.season,
       year: this.year,
-      questions: this.questions
+      questions: this.questions,
     });
   }
 
   async writeToFile() {
-    if (!fs.existsSync(rootPath + "/output"))
-      fs.mkdirSync(rootPath + "/output");
+    if (!fs.existsSync(rootPath + "/output")) fs.mkdirSync(rootPath + "/output");
 
     await this.downloadImages();
 
@@ -117,7 +135,7 @@ export const parseFlow = (flow: Flow, metadata: ExamSetMetadata): ExamSet => {
   examSet.activityId = flow.activityId;
   examSet.fillMetadata(metadata);
   const questionsRaw: Question[] = [];
-  flow.items.forEach(item => {
+  flow.items.forEach((item) => {
     // enkelte sæt har en "intro-tekst" til hvert spørgsmål i item.features
     // -- dette kræver at der kun er 1 spørsmål til hver item, derfor
     //    nedenstående fix
@@ -126,7 +144,7 @@ export const parseFlow = (flow: Flow, metadata: ExamSetMetadata): ExamSet => {
         item.features + "<p></p>" + item.questions[0].data.stimulus;
     }
 
-    item.questions.forEach(question => questionsRaw.push(question));
+    item.questions.forEach((question) => questionsRaw.push(question));
   });
   const questions = questionsRaw.map((question, index) =>
     parseQuestion(question, index + 1)
